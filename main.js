8afb4009a7390ba2737600213aa6eb336ac959a6
javi <main@mail.local>
2023-08-14 18:59:43 -0500

    Fusion No.3 curso practicojs platziC21 clase13 al index

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
-const menuEmail = document.querySelector('.navbar-right');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector(".navbar-right");
+const desktopMenu = document.querySelector(".desktop-menu");
 
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
+const menuCarroIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
+// 1
+menuEmail.addEventListener("click", toggleDesktopMenu);
+// 2
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+// 3
+menuCarroIcon.addEventListener("click", toggleMenuCarroAside);
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
+//1
+function toggleDesktopMenu() {
+//   const isAsideClosed = aside.classList.contains("inactive");
 
-menuHamIcon.addEventListener('click', toggleMobileMenu);
+//   if (!isAsideClosed) {
+//     aside.classList.add("inactive");
+//   }
+  desktopMenu.classList.toggle("inactive");
+};
 
+//2
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+  if (!isAsideClosed) {
+    aside.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+};
+
+//3
+function toggleMenuCarroAside() {
+    const ismobileMenuClosed = mobileMenu.classList.contains("inactive");
+    if (!ismobileMenuClosed) {
+      mobileMenu.classList.add("inactive");
+    }
+
+  aside.classList.toggle("inactive");
 }
 
-function toggleMobileMenu (){
-    mobileMenu.classList.toggle('inactive');
-}
+
